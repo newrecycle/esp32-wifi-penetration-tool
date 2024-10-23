9e51c20a8db98f6e200c22c34a4e29b6cb1c4fff
newrecycle <33557350+newrecycle@users.noreply.github.com>
2024-10-23 04:01:54 -0600

    Update bt_logs_forwarder.h

--- components/bluetooth_serial/bt_logs_forwarder.h
+++ components/bluetooth_serial/bt_logs_forwarder.h
@@ -3,19 +3,20 @@
 
 #include <stdarg.h>
 
-// This class enables all logs, printed by ESP32, to be copied to Bluetooth
+// This class enables all logs, printed by ESP32, to be copied to Bluetooth (now BLE)
 class BtLogsForwarder {
  public:
   BtLogsForwarder();
   void startForwarding();
   void stopForwarding();
 
+  // Forwards log data to the BLE characteristic instead of serial (when BLE is active)
   void printToSerial(const char* format, va_list& args);
 
  private:
   using ESPLogFn = int (*)(const char*, va_list);
 
-  // This function have to be static so that we can use it in ESP Log API
+  // This function must be static so that we can use it in the ESP Log API
   static int btVprintf(const char* format, va_list args);
 
   static ESPLogFn mSerialVprintf;
